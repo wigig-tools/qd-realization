d9933616652f9959ea0bf429b3a1efc684838e3c
Mattia Lecci <mattia.lecci@gmail.com>
2019-09-30 15:09:58 +0200

    Move visualizer to src folder

--- src/visualizer/raysAppFunctions/updateScenario.m
+++ src/visualizer/raysAppFunctions/updateScenario.m
@@ -4,9 +4,9 @@ scenarioName = app.ScenarioDropdown.Value;
 app.scenarioName = scenarioName;
 app.UIAxes.Title.String = scenarioName;
 
-app.outputPath = sprintf('../%s/Output', scenarioName);
-app.visualizerPath = sprintf('../%s/Output/Visualizer', scenarioName);
-app.ns3Path = sprintf('../%s/Output/Ns3', scenarioName);
+app.outputPath = sprintf('../examples/%s/Output', scenarioName);
+app.visualizerPath = sprintf('../examples/%s/Output/Visualizer', scenarioName);
+app.ns3Path = sprintf('../examples/%s/Output/Ns3', scenarioName);
 
 raysAppPlotRoom(app)
 
