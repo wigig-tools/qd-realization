a5df867f61197947625bad16f6fcad113a363849
Mattia Lecci <mattia.lecci@gmail.com>
2020-01-17 17:25:12 +0100

    Restyle of nodeProfileCfg.m
    Tried to put some order in this function.
    Added warnings whenever input cfg parameters are changed.
    No functionalities should have changed.

--- src/nodeProfileCfg.m
+++ src/nodeProfileCfg.m
@@ -4,8 +4,6 @@ function [paraCfg, nodeCfg] = nodeProfileCfg(paraCfg)
 
 scenarioNameStr = paraCfg.inputScenarioName;
 % Input Parameters to be Updated
-environmentFileName = paraCfg.environmentFileName;
-% scenarioNameStr = paraCfg.scenarioNameStr;
 mobilitySwitch = paraCfg.mobilitySwitch;
 mobilityType = paraCfg.mobilityType;
 numberOfNodes = paraCfg.numberOfNodes;
@@ -31,7 +29,10 @@ if switchRandomization == 1
     zCoordinateRandomizer = 1.6;
     Rx = [xCoordinateRandomizer, yCoordinateRandomizer, zCoordinateRandomizer];
     
-    mobilityType = 1;
+    if mobilityType ~= 1
+        mobilityType = 1;
+        warning('Changing mobilityType to %s', mobilityType)
+    end
 end
 
 %% Extracting data from nodes.dat and nodeVelocities.dat file.
@@ -43,18 +44,21 @@ if switchRandomization == 0
         nodeVelocities = csvread(fullfile(inputPath, 'nodeVelocities.dat'));
     catch
         switchRandomization = 1;
+        warning(['Unable to read nodes.dat or nodeVelocities.dat. ',...
+            'Changing switchRandomization to %d'], switchRandomization)
     end
     
     if size(nodeLoc,1) ~= size(nodeVelocities,1) && mobilitySwitch == 1
         error(['nodes.dat and nodeVelocities.dat do not have same number ',...
             'of rows. Please check the input files in the Input folder.'])
     end
-    if ~isempty(numberOfNodes) && numberOfNodes ~= size(nodeLoc,1)
+    
+    if ~isempty(numberOfNodes) && numberOfNodes ~= size(nodeLoc, 1)
         warning(['"numberOfNodes" parameter does not match the number of ',...
             'nodes given in file. The "numberOfNodes" is adjusted to ',...
-            'the number of nodes given in file']);
+            'the number of nodes given in file (%d)'], size(nodeLoc, 1));
     end
-    numberOfNodes = size(nodeLoc,1);
+    numberOfNodes = size(nodeLoc, 1);
     
     if mobilitySwitch == 1
         nodeVelocitiesTemp = nodeVelocities;
@@ -64,35 +68,39 @@ if switchRandomization == 0
         clear nodeVelocities;
         nodeVelocities = zeros(numberOfNodes, 3);
     end
+    
     if mobilityType == 2
         listing = dir(fullfile(scenarioNameStr, 'Input'));
-        sizeListing = size(listing);
+        
         countListing = 0;
-        for iterateSizeListing = 1:sizeListing(1)
+        for iterateSizeListing = 1:size(listing, 1)
             ln = listing(iterateSizeListing).name;
             
             for iterateNumberOfNodes = 1:numberOfNodes
                 if strcmp(ln, sprintf('NodePosition%d.dat', iterateNumberOfNodes))
                     nodePositionTemp = load(fullfile(inputPath, ln));
+                    
                     try
                         nodePosition(:, :, iterateNumberOfNodes) = nodePositionTemp;
                         countListing = countListing + 1;
                     catch
-                        warning(['Node Position input incorrect. Linear',...
-                            'mobility model is chosen']);
                         mobilityType = 1;
+                        warning('Node Position input incorrect. Changing mobilityType to 1');
                     end
+                    
                 end
             end
         end
-        sizeNodePosition = size(nodePosition);
+        
         if mobilityType == 2 && countListing < numberOfNodes
             warning(['Node Position input incorrect. Linear mobility',...
                 'model is chosen']);
             mobilityType = 1;
         elseif mobilityType == 2 && countListing == numberOfNodes
-            numberOfTimeDivisions = sizeNodePosition(1) - 2;
+            numberOfTimeDivisions = size(nodePosition, 1) - 2;
+            warning('Changing numberOfTimeDivisions to %d', numberOfTimeDivisions)
         end
+        
     end
 end
 
@@ -100,32 +108,37 @@ iterateNumberOfNodes = 1;
 %% This part of code generates other parameters of
 nodeAntennaOrientation = zeros(numberOfNodes, 3, 3);
 nodePolarization = zeros(iterateNumberOfNodes, 2);
+
 while iterateNumberOfNodes <= numberOfNodes
     nodeAntennaOrientation(iterateNumberOfNodes, :, :) = [1, 0, 0; 0, 1, 0; 0, 0, 1];
     nodePolarization(iterateNumberOfNodes, :) = [1, 0];
+    
     if switchRandomization == 1 && iterateNumberOfNodes > 0
         xCoordinateRandomizer = rand * 8 + 1;
         yCoordinateRandomizer = rand * 17 + 1;
         zCoordinateRandomizer = 1.6;
-        nodeLoc(iterateNumberOfNodes, :) = [xCoordinateRandomizer, yCoordinateRandomizer, zCoordinateRandomizer];
+        nodeLoc(iterateNumberOfNodes, :) =...
+            [xCoordinateRandomizer, yCoordinateRandomizer, zCoordinateRandomizer];
+        
         xCoordinateRandomizer = rand * 0.7;
         yCoordinateRandomizer = sqrt((0.7^2) - (xCoordinateRandomizer^2));
         zCoordinateRandomizer = 0;
-        nodeVelocities(iterateNumberOfNodes, :) = [xCoordinateRandomizer, yCoordinateRandomizer, zCoordinateRandomizer];
+        nodeVelocities(iterateNumberOfNodes, :) =...
+            [xCoordinateRandomizer, yCoordinateRandomizer, zCoordinateRandomizer];
     end
+    
     iterateNumberOfNodes = iterateNumberOfNodes + 1;
 end
 
 switchRandomization = 0;
+warning('Changing switchRandomization to %d', switchRandomization)
 
 % Check Temp Output Folder
-status = rmdir(fullfile(scenarioNameStr, 'Output'), 's');
+rmdir(fullfile(scenarioNameStr, 'Output'), 's');
 
 mkdir(fullfile(scenarioNameStr, 'Output'));
 mkdir(fullfile(scenarioNameStr, 'Output/Ns3'));
 mkdir(fullfile(scenarioNameStr, 'Output/Visualizer'));
-
-sizeNode = size(nodeLoc);
 
 if ~isfolder(nodesPositionPath)
     mkdir(nodesPositionPath)
