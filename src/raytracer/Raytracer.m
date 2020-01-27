wigig-tools/qd-realization

commit 2637a3d05c3333eb9f7b7dcd8a9dd299c67cb3f8

Slightly change numberOfTimeDivisions
If using mobilityType==2, numerOfTimeDivisions will be equal to the length of the given traces minus 1 (instead of minus 2), while Raytracer will loop numberOfTimeDivisions times instead of numberOfTimeDivisions plus 1.
This effectively changes nothing, but helps with code readability.

src/raytracer/Raytracer.m
@@ -186,7 +186,7 @@ end
 % planes. If that occurs then the velocities are simply reversed (not
 % reflected). At every time step the positions of all nodes are updated
 
-for iterateTimeDivision = 0:numberOfTimeDivisions
+for iterateTimeDivision = 0:numberOfTimeDivisions-1
     
     if mobilityType == 1
         if numberOfNodes == 2

src/nodeProfileCfg.m
@@ -97,7 +97,8 @@ if switchRandomization == 0
                 'model is chosen']);
             mobilityType = 1;
         elseif mobilityType == 2 && countListing == numberOfNodes
-            numberOfTimeDivisions = size(nodePosition, 1) - 2;
+            % Cannot compute last velocity, so stop one iteration earlier
+            numberOfTimeDivisions = size(nodePosition, 1) - 1;
             warning('Changing numberOfTimeDivisions to %d', numberOfTimeDivisions)
         end
         

commit ff7d236ba924ab39f924ead7f2a1cff48f5add4e

Bugfix: set numberOfTimeDivisions=1 if mobilitySwitch~=1
Commit 2637a3d05c3333eb9f7b7dcd8a9dd299c67cb3f8 introduced an unexpected error for mobilitySwitch~=1.
Since the loop over time divisions now goes from 0 to  numberOfTimeDivisions-1, numberOfTimeDivisions has be >0 to run.

src/raytracer/Raytracer.m
@@ -177,7 +177,7 @@ RxInitial = Rx;
 if mobilitySwitch == 1
     timeDivisionValue = totalTimeDuration / numberOfTimeDivisions;
 else
-    numberOfTimeDivisions = 0;
+    numberOfTimeDivisions = 1;
     timeDivisionValue = 0;
 end
 