4051bd819ae506e175bb698be528121008a07d43
Mattia Lecci <mattia.lecci@gmail.com>
2020-02-05 18:53:49 +0100

    Update licenses and documentation

--- src/launchRaytracer.m
+++ src/launchRaytracer.m
@@ -11,6 +11,22 @@ function outputPath = launchRaytracer(varargin)
 %LAUNCHRAYTRACER(__, 'verbose', v): 'verbose' parameter with level v. If
 %v==0, no information is written on the command window. Default: v = 1.
 
+
+% Copyright (c) 2020, University of Padova, Department of Information
+% Engineering, SIGNET lab.
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+% you may not use this file except in compliance with the License.
+% You may obtain a copy of the License at
+%
+%    http://www.apache.org/licenses/LICENSE-2.0
+%
+% Unless required by applicable law or agreed to in writing, software
+% distributed under the License is distributed on an "AS IS" BASIS,
+% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+% See the License for the specific language governing permissions and
+% limitations under the License.
+
 % Input handling
 p = inputParser;
 
