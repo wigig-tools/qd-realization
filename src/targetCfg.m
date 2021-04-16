7fedafb45195c0b19f7dfc88a6301fee0f0979e2
Steve Blandino <steve.blandino@nist.gov>
2021-04-16 09:21:10 -0400

    Target config code improvement

--- src/targetCfg.m
+++ src/targetCfg.m
@@ -1,6 +1,6 @@
 function  trgCfg  = targetCfg(paraCfg)
 %TARGETCFG import target configuration.
-%   [P, T] = TARGETCFG(P) imports the target configuration parameter struct
+%   T = TARGETCFG(P) imports the target configuration parameter struct
 %   T given the system configuration struct P
 %
 
@@ -40,6 +40,7 @@ function  trgCfg  = targetCfg(paraCfg)
 %
 % Modified by: Steve Blandino <steve.blandino@nist.gov>
 
+%% Init operations
 % Find list of file in input folder
 scenarioNameStr = paraCfg.inputScenarioName;
 inputPath = fullfile(scenarioNameStr, 'Input');
@@ -51,63 +52,53 @@ numberOfTargets = sum(arrayfun(@(x) startsWith(x.name,'TargetBase'), ...
 trgtBase = cell(numberOfTargets,1);
 trgtJoint = cell(numberOfTargets,1);
 trgtJointsMat = cell(numberOfTargets,1);
-trgtRotationTime= zeros(paraCfg.numberOfTimeDivisions,3, numberOfTargets);
-trgtPositionTime = zeros(paraCfg.numberOfTimeDivisions,3, numberOfTargets);
 trgtJoints = zeros(numberOfTargets,1);
 
 %% Loop over target position files
 for trgtId = 1:numberOfTargets
-    %Check if files relative to trgtId are defined
+    % Load target base file relative to trgtId
     trgtBaseFile = sprintf('TargetBase%d.dat',trgtId-1);
-    trgtJointsFile = sprintf('TargetJoints%d.dat',trgtId-1);
     isTrgtBase = any(arrayfun(@(x) strcmp(x.name,trgtBaseFile), ...
         fileList));
-    isTrgtJoints = any(arrayfun(@(x) strcmp(x.name,trgtJointsFile), ...
-        fileList));
-    
-    if ~isTrgtBase
-        error([ trgtBaseFile, ' not defined.']);
-    end
-    
+    assert(isTrgtBase, [ trgtBaseFile, ' not defined.']);
     %Load Target position
     trgtBase{trgtId} = readmatrix(fullfile(inputPath,...
         trgtBaseFile));
     trgtTimeSamples = size(trgtBase{trgtId},1);
     
+    % Load target joints file
+    trgtJointsFile = sprintf('TargetJoints%d.dat',trgtId-1);
+    isTrgtJoints = any(arrayfun(@(x) strcmp(x.name,trgtJointsFile), ...
+        fileList));
+
     if isTrgtJoints
         trgtJoint{trgtId} = readmatrix(fullfile(inputPath,...
             trgtJointsFile));
-        assert(trgtTimeSamples == size(trgtJoint{trgtId},1), 'Base and joints need to have the same temporal size')
+        assert(trgtTimeSamples == size(trgtJoint{trgtId},1), ...
+            'Base and joints need to have the same temporal size')
         numJoints = size(trgtJoint{trgtId},2)/3;
-        trgtJointsMat{trgtId} = reshape(trgtJoint{trgtId}, trgtTimeSamples, 3, numJoints);
+        trgtJointsMat{trgtId} = reshape(trgtJoint{trgtId}, ...
+            trgtTimeSamples, 3, numJoints); % Time x 3 x numJoints
     else
-        trgtTimeSamples = 1;
+        numJoints = 1;
         trgtJoint{trgtId} = repmat([0 0 0], trgtTimeSamples,numJoints);
         writematrix(trgtJoint{trgtId}, fullfile(inputPath,...
             trgtJointsFile));
-        warning([trgtJointsFile, ' not defined. Static target'])
+        warning([trgtJointsFile, ' not defined.'])
     end
     
-    % Target position points in time needs to match node position
-    % information
-    if  trgtTimeSamples~= paraCfg.numberOfTimeDivisions &&  ...
-            paraCfg.numberOfTimeDivisions > 1
-        error('Target number of points do not match config description')
+    % If node are mobile Target position points in time needs to match node
+    % position information
+    if paraCfg.nodeMobility 
+        assert(trgtTimeSamples== paraCfg.numberOfTimeDivisions, ...
+            'Target number of points do not match config description')
     end
     
-    %     trgtPositionTime(1:trgtTimeSamples, :, trgtId) = ...
-    %         trgtInitialPosition{trgtId}(1:trgtTimeSamples,:);
-    %     trgtPositionTime(trgtTimeSamples+1:end, :, trgtId) = ...
-    %         repmat(trgtInitialPosition{trgtId}(1,:), [paraCfg.numberOfTimeDivisions-1,1,1]);
-    %
-    %     trgtRotationTime(1:trgtTimeSamples, :, trgtId) = ...
-    %         trgtDynamics{trgtId}(1:trgtTimeSamples,:);
-    %     trgtRotationTime(trgtTimeSamples+1:end, :, trgtId) = ...
-    %         repmat(trgtDynamics{trgtId}(1,:), [paraCfg.numberOfTimeDivisions-1,1,1]);
-    
-    trgtJoints(trgtId) = numJoints+1;
-    trgtJointsMat{trgtId} = cat(3, trgtBase{trgtId}(:,1:3),  trgtJointsMat{trgtId} );
+    trgtJoints(trgtId) = numJoints+1; % Add base
+    trgtJointsMat{trgtId} = cat(3, trgtBase{trgtId}(:,1:3),...
+    trgtJointsMat{trgtId} );
 end
+
 trgtJointsMat = cat(3, trgtJointsMat{:});
 trgtPositionTime = trgtJointsMat(:, 1:3,:);
 trgtRotationTime = cat(3, trgtBase{:});
