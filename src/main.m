c8f27555a54d809b199012af22723901de8ea7ab
Neeraj Varshney <neeraj.varshney@nist.gov>
2021-05-07 09:42:07 -0400

    Update main.m

--- src/main.m
+++ src/main.m
@@ -54,7 +54,7 @@ end
 
 %% Input
 % Leave empty for default 'ScenarioTest'
-scenarioNameStr = 'examples/EnterpriseCubicle';
+scenarioNameStr = 'examples/BoxLectureRoom';
 
 if ~isempty(scenarioNameStr)
     fprintf('Use customized scenario: %s.\n',scenarioNameStr);
